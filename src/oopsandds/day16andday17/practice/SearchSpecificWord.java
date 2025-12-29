a0eaadfd4a6a5dfd3888217ea8acb3900c020553
cheenum <38590526+cheenum@users.noreply.github.com>
2025-12-29 15:57:53 +0530

    Day 16 and 17 SearchSpecificWord

---
+++ src/oopsandds/day16andday17/practice/SearchSpecificWord.java
@@ -0,0 +1,23 @@
+package oopsandds.day16andday17.practice;
+
+public class SearchSpecificWord {
+    private static String findSearchSpecificWord(String[] strs, String word) {
+        for (String str : strs) {
+            if (str.contains(word)) {
+                return str;
+            }
+        }
+        return "Not found";
+    }
+
+    public static void main(String[] args) {
+        String[] str = {
+                "Java is powerful",
+                "Data Structures are important",
+                "Programming makes problem solving easier"
+        };
+        String word = "makes";
+        String results = findSearchSpecificWord(str, word);
+        System.out.println("Specific Word in sentence: " + results);
+    }
+}
