c24d1406980f1c080fe6a1d95390634e736ebb7e
cheenum <38590526+cheenum@users.noreply.github.com>
2025-12-31 11:09:30 +0530

    Day 19 User Registration UC2

--- src/com/bl/advance/java/UserRegistration.java
+++ src/com/bl/advance/java/UserRegistration.java
@@ -1,7 +1,29 @@
 package com.bl.advance.java;
 
-public class UserRegistration {
-    public static void main(String[] args) {
+import java.util.regex.Pattern;
 
+public class UserRegistration {
+
+    // UC1
+    private static void UC1() {
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+        String name = "Cheenu";
+        boolean isValidName = Pattern.matches(regex, name);
+        System.out.println("Is that valid name: " + isValidName);
     }
+
+    // UC2
+    private static void UC2() {
+        String regex = "^(?:\\S+\\s)+[A-z][a-zA-Z]{2,}$";
+        String input = "cheenu M";
+        boolean isValidLastname = Pattern.matches(regex, input);
+        System.out.println("Is that Last name is valid: " + isValidLastname);
+    }
+
+    public static void main(String[] args) {
+        UC1();
+        UC2();
+    }
+
+
 }
