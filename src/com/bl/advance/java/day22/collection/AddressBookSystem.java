b5c1469a0a58fe3ea91c7fa82aa18d61532624e0
cheenum <38590526+cheenum@users.noreply.github.com>
2026-01-03 12:26:57 +0530

    day 22 Address Book UC1

--- src/com/bl/advance/java/day22/collection/AddressBookSystem.java
+++ src/com/bl/advance/java/day22/collection/AddressBookSystem.java
@@ -3,5 +3,19 @@ package com.bl.advance.java.day22.collection;
 public class AddressBookSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book...");
+
+        Contacts contact = new Contacts(
+                "demo",
+                "Doe",
+                "Street 123",
+                "Chennai",
+                "TamilNadu",
+                "411001",
+                "9876543210",
+                "demo@gmail.com"
+        );
+
+        System.out.println("Contact Created Successfully:");
+        contact.showContact();
     }
 }
