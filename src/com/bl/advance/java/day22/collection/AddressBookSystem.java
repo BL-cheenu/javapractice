9bc59c0b2c9b3b3980437561ba5371c99ae82665
cheenum <38590526+cheenum@users.noreply.github.com>
2026-01-03 13:57:18 +0530

    day 22 Address Book UC3

--- src/com/bl/advance/java/day22/collection/AddressBookSystem.java
+++ src/com/bl/advance/java/day22/collection/AddressBookSystem.java
@@ -2,9 +2,11 @@ package com.bl.advance.java.day22.collection;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AddressBookSystem {
     List<Contacts> contactsArrayList = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
 
     public void addContact(Contacts contacts) {
         contactsArrayList.add(contacts);
@@ -15,5 +17,41 @@ public class AddressBookSystem {
         for (Contacts contacts : contactsArrayList) {
             contacts.showContact();
         }
+    }
+
+    public void editContact() {
+        System.out.print("Enter First name of contact to edit: ");
+        String name = sc.nextLine();
+
+        for (Contacts contact : contactsArrayList) {
+            if (contact.firstName.equalsIgnoreCase(name)) {
+                System.out.println("Contact Found! Enter new details:");
+
+                System.out.print("Enter Last Name : ");
+                contact.lastName = sc.nextLine();
+
+                System.out.print("Enter Address : ");
+                contact.address = sc.nextLine();
+
+                System.out.print("Enter City : ");
+                contact.city = sc.nextLine();
+
+                System.out.print("Enter State : ");
+                contact.state = sc.nextLine();
+
+                System.out.print("Enter Zip : ");
+                contact.zip = sc.nextLine();
+
+                System.out.print("Enter Phone Number : ");
+                contact.phoneNumber = sc.nextLine();
+
+                System.out.print("Enter Email : ");
+                contact.email = sc.nextLine();
+
+                System.out.print("Contact Updated Successfully!");
+                return;
+            }
+        }
+        System.out.println("Contact Not Found!");
     }
 }
--- src/com/bl/advance/java/day22/collection/AddressBookSystemMain.java
+++ src/com/bl/advance/java/day22/collection/AddressBookSystemMain.java
@@ -7,33 +7,60 @@ public class AddressBookSystemMain {
         Scanner sc = new Scanner(System.in);
         AddressBookSystem book = new AddressBookSystem();
 
-        System.out.print("Enter First Name : ");
-        String firstName = sc.nextLine();
+        while (true) {
+            System.out.println("--- Address Book Menu ---");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Edit Contact");
+            System.out.println("3. Show Contacts");
+            System.out.println("4. Exit");
 
-        System.out.print("Enter Last Name : ");
-        String lastName = sc.nextLine();
+            System.out.print("Enter choice : ");
+            int choice = sc.nextInt();
+            sc.nextLine();
 
-        System.out.print("Enter Address : ");
-        String address = sc.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter First Name : ");
+                    String firstName = sc.nextLine();
 
-        System.out.print("Enter City : ");
-        String city = sc.nextLine();
+                    System.out.print("Enter Last Name : ");
+                    String lastName = sc.nextLine();
 
-        System.out.print("Enter State : ");
-        String state = sc.nextLine();
+                    System.out.print("Enter Address : ");
+                    String address = sc.nextLine();
 
-        System.out.print("Enter Zip : ");
-        String zip = sc.nextLine();
+                    System.out.print("Enter City : ");
+                    String city = sc.nextLine();
 
-        System.out.print("Enter Phone Number : ");
-        String phone = sc.nextLine();
+                    System.out.print("Enter State : ");
+                    String state = sc.nextLine();
 
-        System.out.print("Enter Email : ");
-        String email = sc.nextLine();
+                    System.out.print("Enter Zip : ");
+                    String zip = sc.nextLine();
 
-        Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phone, email);
-        book.addContact(contact);
+                    System.out.print("Enter Phone Number : ");
+                    String phone = sc.nextLine();
 
-        book.showALlContacts();
+                    System.out.print("Enter Email : ");
+                    String email = sc.nextLine();
+
+                    Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phone, email);
+                    book.addContact(contact);
+
+                    break;
+                case 2:
+                    book.editContact();
+                    break;
+
+                case 3:
+                    book.showALlContacts();
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+            }
+        }
+
+
     }
 }
