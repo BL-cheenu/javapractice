70203251da8f4c5d9c0c3df0193e797e3923f6e8
cheenum <38590526+cheenum@users.noreply.github.com>
2026-01-13 16:31:10 +0530

    day 29 UC1 TC1.1

--- src/main/java/com/bl/day29/practice/CensusMain.java
+++ src/main/java/com/bl/day29/practice/CensusMain.java
@@ -1,13 +1,15 @@
 package com.bl.day29.practice;
 
+import com.bl.exception.StateCensusException;
+
 public class CensusMain {
     public static void main(String[] args) {
 
         try {
             StateCensusAnalyser analyser = new StateCensusAnalyser();
-            int count = analyser.loadIndiaStateCensusData( "D:\\Workspace\\IndiaStateCensusData.csv");
+            int count = analyser.loadIndiaStateCensusData("D:\\Workspace\\IndiaStateCensusData.csv");
             System.out.println("Total Records Loaded: " + count);
-        } catch (Exception e) {
+        } catch (StateCensusException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/com/bl/day29/practice/StateCensusAnalyser.java
+++ src/main/java/com/bl/day29/practice/StateCensusAnalyser.java
@@ -1,5 +1,6 @@
 package com.bl.day29.practice;
 
+import com.bl.exception.StateCensusException;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
@@ -9,23 +10,31 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class StateCensusAnalyser {
-    public int loadIndiaStateCensusData(String csvFilePath) throws Exception {
+    public int loadIndiaStateCensusData(String csvFilePath)
+            throws StateCensusException {
 
-        Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
 
-        CsvToBean<CSVStateCensus> csvToBean =
-                new CsvToBeanBuilder<CSVStateCensus>(reader)
-                        .withType(CSVStateCensus.class)
-                        .withIgnoreLeadingWhiteSpace(true)
-                        .build();
+            CsvToBean<CSVStateCensus> csvToBean =
+                    new CsvToBeanBuilder<CSVStateCensus>(reader)
+                            .withType(CSVStateCensus.class)
+                            .withIgnoreLeadingWhiteSpace(true)
+                            .build();
 
-        Iterator<CSVStateCensus> iterator = csvToBean.iterator();
+            Iterator<CSVStateCensus> iterator = csvToBean.iterator();
 
-        int count = 0;
-        while (iterator.hasNext()) {
-            iterator.next();
-            count++;
+            int count = 0;
+            while (iterator.hasNext()) {
+                iterator.next();
+                count++;
+            }
+            return count;
+
+        } catch (Exception e) {
+            throw new StateCensusException("Invalid file path",
+                    StateCensusException.ExceptionType.FILE_NOT_FOUND
+            );
         }
-        return count;
     }
 }
