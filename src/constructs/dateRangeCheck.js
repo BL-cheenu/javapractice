f1ab8d9bd30c107ce0f75a6fd0c3c5f5dd4d7aa7
cheenum <38590526+cheenum@users.noreply.github.com>
2026-01-27 12:50:01 +0530

    Added program to check if a given date falls between March 20 and June 20

---
+++ src/constructs/dateRangeCheck.js
@@ -0,0 +1,8 @@
+let day = 15;
+let month = "April";
+let isTrue = false;
+
+if ((month === "March" && day >= 20 ) || (month === "April") || (month === "May" ) || (month === "June" && day <= 20)){
+    isTrue = true;
+}
+console.log(isTrue);
